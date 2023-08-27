f78947c3d8c66923dbf13439269d4b4f8f7fa26d
Noel Pascal Schwabenland <noel@schwabenland.ch>
2023-08-27 22:00:37 +0200

    fixed gif image order

--- src/make_training_gif.py
+++ src/make_training_gif.py
@@ -10,12 +10,12 @@ fp_out = "training.gif"
 with contextlib.ExitStack() as stack:
 
     # lazily load images
-    imgs = (stack.enter_context(Image.open(f))
-            for f in sorted(glob.glob(fp_in)))
+    imgs = (stack.enter_context(Image.open(f)) for f in sorted(glob.glob(fp_in)))
 
-    # extract  first image from iterator
-    img = next(imgs)
+    images = list(imgs) # make list of images from generator
 
+    # Sort the images and make sure 002 is before 020
+    images.sort(key=lambda x: int(x.filename.split("\\")[-1].split(".")[0].split("_")[-1])) # Only works on windows
+    
     # https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#gif
-    img.save(fp=fp_out, format='GIF', append_images=imgs,
-             save_all=True, duration=200, loop=0)
+    images[0].save(fp=fp_out, format='GIF', append_images=images[1:], save_all=True, duration=20, loop=0)
