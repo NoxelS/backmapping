b9eccaab7c66ecb71417aa195d23497636b78f07
Noel Pascal Schwabenland <noel@schwabenland.ch>
2023-08-27 18:23:50 +0200

    script now removes dirs

--- run.bat
+++ run.bat
@@ -1,4 +1,8 @@
 @echo off
 
-start "Training" cmd /c "python src/train.py > output.log"
+rmdir /s /q data\\tensorboard
+rmdir /s /q data\\backup
+rmdir /s /q data\\hist
+
+start "Training" cmd /c "python src/train.py"
 start "Tensorboard" cmd /c "tensorboard --logdir=data/tensorboard --host 0.0.0.0 --port 6006"
