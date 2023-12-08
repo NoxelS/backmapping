b4f777eac2c509ce570a5d3f72888be03dd606d0
Noel Schwabenland <noel@schwabenland.ch>
2023-12-08 14:38:03 +0100

    removed unused script

--- run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-@REM rmdir /s /q data\\tensorboard
-@REM rmdir /s /q data\\backup
-@REM rmdir /s /q data\\hist
-
-start "Training" cmd /c "python src/train.py"
-start "Tensorboard" cmd /c "tensorboard --logdir=data/tensorboard --host 0.0.0.0 --port 6006"
-@REM pdoc --html src/library
