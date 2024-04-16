2a118edbb4f98f796f5e3358b1eb6b4f997945fd
Noel Pascal Schwabenland <noel@schwabenland.ch>
2024-04-16 12:21:41 +0200

    fixed memdof script paths

--- src/gen_train_data.py
+++ src/gen_train_data.py
@@ -1,10 +1,10 @@
-from library.datagen.misc import unzip_raw_files
 from library.datagen.membranes import generate_membrane_data
+from library.datagen.misc import unzip_raw_files
 from library.datagen.molecules import generate_molecule_data
 from library.datagen.topology import generate_extended_topology
 
 # We use a 10:1 ratio for training and validation data so the batch sizes are optimal
-MAX_TRAINING_DATA = 15 * (10 + 1) * 1024 
+MAX_TRAINING_DATA = 1 * (10 + 1) * 1024
 
 """
     First, we unzip the raw data and move it to a dedicated folder
@@ -20,14 +20,14 @@ generate_membrane_data()
 
 
 """
-    Now we use the membranes to generate the molecule data. This will create a folder with all the molecules in the
-    <DATA_PREFIX>/training folder.
-"""
-generate_molecule_data(MAX_TRAINING_DATA) 
-
-
-"""
     Use memdof to generate the extended topology of the molecule. This will include all internal coordinates and a flag
     that indicates if the internal coordinate needs to be fitted or not.
 """
 generate_extended_topology()
+
+
+"""
+    Now we use the membranes to generate the molecule data. This will create a folder with all the molecules in the
+    <DATA_PREFIX>/training folder.
+"""
+generate_molecule_data(MAX_TRAINING_DATA)
--- src/library/datagen/topology.py
+++ src/library/datagen/topology.py
@@ -1,14 +1,15 @@
-from memdof import parse_PTB, calc_IDOF, parse_topology, ExtendedTopologyInfo
-from library.config import Keys, config
-import pickle
 import os
+import pickle
 
+from memdof import ExtendedTopologyInfo, calc_IDOF, parse_PTB, parse_topology
+
+from library.config import Keys, config
 
 # Load paths from the config
-PATH_TO_TOPOLOGY = 'data/topologies/DOPC.itp' # Path to the GROMACS topology file (.itp)
-PATH_TO_PDB = os.path.join(config(Keys.DATA_PATH), "membranes", "0", "at.pdb") # Path to the PDB file
-TOPOLOGY_PLOT_PATH = os.path.join(config(Keys.DATA_PATH), "ic_dof_analysis") # Path to the folder where the plots will be saved
-CSV_PATH = os.path.join(config(Keys.DATA_PATH), "ic_dof_analysis") # Path to the folder where the CSV files will be saved
+PATH_TO_TOPOLOGY = "data/topologies/DOPC.itp"  # Path to the GROMACS topology file (.itp)
+PATH_TO_PDB = os.path.join(config(Keys.DATA_PATH), "membranes", "0", "at.pdb")  # Path to the PDB file
+TOPOLOGY_PLOT_PATH = os.path.join(config(Keys.DATA_PATH), "ic_dof_analysis")  # Path to the folder where the plots will be saved
+CSV_PATH = os.path.join(config(Keys.DATA_PATH), "ic_dof_analysis")  # Path to the folder where the CSV files will be saved
 
 
 def generate_extended_topology() -> ExtendedTopologyInfo:
@@ -22,20 +23,21 @@ def generate_extended_topology() -> ExtendedTopologyInfo:
     os.makedirs(CSV_PATH) if not os.path.exists(CSV_PATH) else None
 
     # Parse the topology information from a GROMACS topology file (.itp)
-    topology_info = parse_topology(PATH_TO_TOPOLOGY)
+    topology_info = parse_topology(PATH_TO_TOPOLOGY, ignore_hydrogen=True)
 
     # Parse the PDB file to extract the structure
     structure, pbc = parse_PTB(PATH_TO_PDB)
 
     # Calculate the internal degrees of freedom
-    extended_topology_info: ExtendedTopologyInfo = calc_IDOF(structure, pbc, topology_info, quiet=False, create_plots=True, plots_path=TOPOLOGY_PLOT_PATH, create_csv=True, csv_path=CSV_PATH)
+    extended_topology_info: ExtendedTopologyInfo = calc_IDOF(
+        structure, pbc, topology_info, quiet=False, create_plots=True, plots_path=TOPOLOGY_PLOT_PATH, create_csv=True, csv_path=CSV_PATH
+    )
 
     # Save the extended topology information
-    with open(f"{config(Keys.DATA_PATH)}/training/DOPC/extended_topology.pkl", 'wb') as fp:
+    with open(os.path.join(TOPOLOGY_PLOT_PATH, "extended_topology.pkl"), "wb") as fp:
         pickle.dump(extended_topology_info, fp)
 
     return extended_topology_info
-
 
 
 if __name__ == "__main__":
