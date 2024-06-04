d172b863fe8c0ebe83703d7446844e92aa0ada4b
Noel Schwabenland <noel@schwabenland.ch>
2024-06-04 17:38:09 +0200

    added activations test run

--- create_configs.sh
+++ create_configs.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
 # Loop over 10 integers
-for i in {11..13}
+for i in {2..3}
 do
-    cp data/configs/lr_run_1.ini data/configs/lr_run_${i}.ini
+    cp data/configs/activation_run_1.ini data/configs/activation_run_${i}.ini
 done
 
-sed -i -e 's/0.1/0.05/g' data/configs/lr_run_2.ini
-sed -i -e 's/0.1/0.04/g' data/configs/lr_run_3.ini
-sed -i -e 's/0.1/0.03/g' data/configs/lr_run_4.ini
-sed -i -e 's/0.1/0.02/g' data/configs/lr_run_5.ini
-sed -i -e 's/0.1/0.01/g' data/configs/lr_run_6.ini
-sed -i -e 's/0.1/0.009/g' data/configs/lr_run_7.ini
-sed -i -e 's/0.1/0.008/g' data/configs/lr_run_8.ini
-sed -i -e 's/0.1/0.007/g' data/configs/lr_run_9.ini
-sed -i -e 's/0.1/0.006/g' data/configs/lr_run_10.ini
-sed -i -e 's/0.1/0.5/g' data/configs/lr_run_11.ini
-sed -i -e 's/0.1/1/g' data/configs/lr_run_12.ini
-sed -i -e 's/0.1/2/g' data/configs/lr_run_13.ini
+sed -i -e 's/activation_1/activation_2/g' data/configs/activation_run_2.ini
+sed -i -e 's/linear/sigmoid/g' data/configs/activation_run_2.ini
+
+sed -i -e 's/activation_1/activation_3/g' data/configs/activation_run_3.ini
+sed -i -e 's/linear/tanh/g' data/configs/activation_run_3.ini
