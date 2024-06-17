de8900fdf76fd9d53f28a77ea007c00da27373df
Noel Pascal Schwabenland <noel@schwabenland.ch>
2024-06-17 11:20:18 +0200

    added plots for single histograms back in

--- src/post_analysis_single.py
+++ src/post_analysis_single.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 import os
 import pickle
@@ -12,19 +13,18 @@ from library.config import Keys, config
 from library.datagen.topology import get_ic_from_index, ic_to_hlabel
 from library.plot_config import set_plot_config
 
-import argparse
-
 if not os.path.exists("post_analysis"):
     os.makedirs("post_analysis")
+
 
 def post_analysis(config_name: str, type: str = "mixed"):
     """
     Plot the histograms of the validation predictions for a specific configuration.
-    
+
     Args:
         config_name (str): The name of the configuration to plot.
         type (str, optional): The type of the internal coordinate to plot. Defaults to "mixed". Can be "angle" or "bond".
-    
+
     """
 
     # List all folders in analysis
@@ -45,62 +45,62 @@ def post_analysis(config_name: str, type: str = "mixed"):
         with open(file, "rb") as f:
             predictions[os.path.basename(os.path.dirname(file))] = pickle.load(f, fix_imports=False)
 
-    # for model in predictions.keys():
-    #     print("Model:", model)
+    for model in predictions.keys():
+        print("Model:", model)
 
-    #     ic_index = int(model[::-1].split("_", 1)[0][::-1])
-    #     ic_label = ic_to_hlabel(get_ic_from_index(ic_index))
+        ic_index = int(model[::-1].split("_", 1)[0][::-1])
+        ic_label = ic_to_hlabel(get_ic_from_index(ic_index))
 
-    #     y_true = np.array(predictions[model][:, 1])
-    #     y_pred = np.array(predictions[model][:, 2])
+        y_true = np.array(predictions[model][:, 1])
+        y_pred = np.array(predictions[model][:, 2])
 
-    #     try:
-    #         y_true = [y[0] for y in y_true]
-    #         y_pred = [y[0] for y in y_pred]
-    #     except Exception as _:
-    #         pass
+        try:
+            y_true = [y[0] for y in y_true]
+            y_pred = [y[0] for y in y_pred]
+        except Exception as _:
+            pass
 
-    #     # Normalize the values
-    #     y_true_weights = np.ones_like(y_true) / float(len(y_true))
-    #     y_pred_weights = np.ones_like(y_pred) / float(len(y_pred))
+        # Normalize the values
+        y_true_weights = np.ones_like(y_true) / float(len(y_true))
+        y_pred_weights = np.ones_like(y_pred) / float(len(y_pred))
 
-    #     min_y = min(min(y_true), min(y_pred))
-    #     max_y = max(max(y_true), max(y_pred))
+        min_y = min(min(y_true), min(y_pred))
+        max_y = max(max(y_true), max(y_pred))
 
-    #     # Make two plots, the upper plot shows the two histograms while
-    #     # the lower plot shows the difference between the two histograms
-    #     fig, axs = plt.subplots(2, 1, figsize=(10, 6))
+        # Make two plots, the upper plot shows the two histograms while
+        # the lower plot shows the difference between the two histograms
+        fig, axs = plt.subplots(2, 1, figsize=(10, 6))
 
-    #     # Make upper plot higher
-    #     axs[0].set_position([0.1, 0.5, 0.8, 0.4])
-    #     axs[1].set_position([0.1, 0.1, 0.8, 0.2])
+        # Make upper plot higher
+        axs[0].set_position([0.1, 0.5, 0.8, 0.4])
+        axs[1].set_position([0.1, 0.1, 0.8, 0.2])
 
-    #     set_plot_config(["seaborn-pastel"])
+        set_plot_config(["seaborn-pastel"])
 
-    #     a, bins, _ = axs[0].hist(y_true, bins=50, alpha=0.4, label="True", color="xkcd:blue", edgecolor="black", weights=y_true_weights)
-    #     b, _, _ = axs[0].hist(y_pred, bins=bins, alpha=0.4, label="Predicted", color="xkcd:green", edgecolor="black", weights=y_pred_weights)
-    #     axs[0].legend()
+        a, bins, _ = axs[0].hist(y_true, bins=50, alpha=0.4, label="True", color="xkcd:blue", edgecolor="black", weights=y_true_weights)
+        b, _, _ = axs[0].hist(y_pred, bins=bins, alpha=0.4, label="Predicted", color="xkcd:green", edgecolor="black", weights=y_pred_weights)
+        axs[0].legend()
 
-    #     # Add labels
-    #     axs[0].set_xlabel("Angle (°)" if "angle" in model else "Bond Length (Å)")
-    #     axs[0].set_ylabel("Rel. Frequency")
-    #     axs[0].grid(which="major", linestyle="-", linewidth="0.75", color=[0.1, 0.1, 0.1], alpha=0.5)
-    #     axs[0].grid(which="minor", linestyle="--", linewidth="0.25", color=[0.2, 0.2, 0.2], alpha=0.3)
-    #     title_a = "Bond Length" if "bond" in model else "Angle"
-    #     title = title_a + " Histogram for IC " + ic_label
-    #     axs[0].set_title(title)
+        # Add labels
+        axs[0].set_xlabel("Angle (°)" if "angle" in model else "Bond Length (Å)")
+        axs[0].set_ylabel("Rel. Frequency")
+        axs[0].grid(which="major", linestyle="-", linewidth="0.75", color=[0.1, 0.1, 0.1], alpha=0.5)
+        axs[0].grid(which="minor", linestyle="--", linewidth="0.25", color=[0.2, 0.2, 0.2], alpha=0.3)
+        title_a = "Bond Length" if "bond" in model else "Angle"
+        title = title_a + " Histogram for IC " + ic_label
+        axs[0].set_title(title)
 
-    #     # Lower plot
-    #     axs[1].bar(bins[:-1], height=((a) - (b)), alpha=0.4, color="xkcd:purple", edgecolor="black", width=bins[1] - bins[0])
-    #     axs[1].set_xlabel("Angle (°)" if "angle" in model else "Bond Length (Å)")
-    #     axs[1].set_ylabel("Rel. Frequency Diff.")
+        # Lower plot
+        axs[1].bar(bins[:-1], height=((a) - (b)), alpha=0.4, color="xkcd:purple", edgecolor="black", width=bins[1] - bins[0])
+        axs[1].set_xlabel("Angle (°)" if "angle" in model else "Bond Length (Å)")
+        axs[1].set_ylabel("Rel. Frequency Diff.")
 
-    #     # Set ranges
-    #     axs[0].set_xlim(min_y, max_y)
-    #     axs[1].set_xlim(min_y, max_y)
-    #     axs[1].set_ylim(-0.1, 0.1)
+        # Set ranges
+        axs[0].set_xlim(min_y, max_y)
+        axs[1].set_xlim(min_y, max_y)
+        axs[1].set_ylim(-0.1, 0.1)
 
-    #     plt.savefig(os.path.join("post_analysis", f"{model}_histogram.pdf"))
+        plt.savefig(os.path.join("post_analysis", f"{model}_histogram.pdf"))
 
     # Plot all the histograms in one plot
     fig, axs = plt.subplots(2, 1, figsize=(10, 6))
