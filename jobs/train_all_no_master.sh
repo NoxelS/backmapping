1ce2f4ce2f1ec12bc5b6861b3cb2683a2d6fda20
Noel Schwabenland <noel@schwabenland.ch>
2024-07-02 12:00:34 +0200

    updated gres

--- jobs/train_all_no_master.sh
+++ jobs/train_all_no_master.sh
@@ -5,22 +5,9 @@
 #SBATCH --partition=deflt
 #SBATCH --nice=0
 
-# This block is echoing some SLURM variables
-echo "###################### JOB DETAILS ########################"
-echo "Job execution start: $(date)"
-echo "JobID = $SLURM_JOBID"
-echo "Host = $SLURM_JOB_NODELIST"
-echo "Jobname = $SLURM_JOB_NAME"
-echo "Subcwd = $SLURM_SUBMIT_DIR"
-echo "SLURM_TASKS_PER_NODE = $SLURM_TASKS_PER_NODE"
-echo "SLURM_CPUS_PER_TASK = $SLURM_CPUS_PER_TASK"
-echo "SLURM_CPUS_ON_NODE = $SLURM_CPUS_ON_NODE"
-echo "SLURM_GPUS_PER_TASK = $SLURM_GPUS_PER_TASK"
-echo "SLURM_GPUS_ON_NODE = $SLURM_GPUS_ON_NODE"
-echo ""
 
 date
-echo "####################### CODE ###########################"
+echo "####################### MASTER ###########################"
 
 
 bond_ics=(
@@ -46,9 +33,9 @@ bond_ics=(
     "48"
     "49"
     "52"
-    "53"
 )
 angle_ics=(
+    "53"
     "54"
     "55"
     "56"
@@ -171,10 +158,104 @@ dihedral_ics=(
     "172"
 )
 
+# Read data dir from config.ini
+data_dir=$(grep "data_path" config.ini | cut -d "=" -f 2 | cut -d ";" -f 1 | xargs)
+hist_dir=$data_dir/hist
+
+# Get all the hist files that contain $1 in their name
+hist_files=$(ls $hist_dir | grep prod)
+
+overall_epochs=0
+overall_target_epochs=0
+
+echo "Starting to check the training history files for the target epochs"
+
+# Loop through all the hist files
+for hist_file in $hist_files; do
+    # Get the current epoch
+    epoch=$(tail -n 1 $hist_dir/$hist_file | cut -d " " -f 1 | cut -d "," -f 1)
+    # Get the current val_loss
+    val_loss=$(tail -n 1 $hist_dir/$hist_file | cut -d " " -f 4 | cut -d "," -f 6)
+
+    # Get the config name out of the hist name
+    config_name=$(echo $hist_file | sed 's/training_history_//g')
+    config_name=$(echo $config_name | sed 's/.csv//g')
+    config_name=$(echo $config_name | sed 's/_[0-9]*$//g')
+
+    # Get the config file that has model_name_prefix=config_name
+    conf_file=$(grep -l "model_name_prefix = $config_name" data/configs/*)
+
+    # Read the target epoch from the config
+    target_epoch=$(grep "epochs" $conf_file | cut -d "=" -f 2 | xargs)
+
+    # Set epoch and val loss 0 if not found
+    if [ -z "$epoch" ]; then
+        epoch=0
+    fi
+
+    if [ -z "$val_loss" ]; then
+        val_loss=0
+    fi
+
+    # Add one epoch to $epoch
+    epoch=$((epoch + 1))
+
+    # If epoch is bigger than target epoch, set it to target epoch
+    if [ $epoch -gt $target_epoch ]; then
+        epoch=$target_epoch
+    fi
+
+    # Remove "train_" from the hist_file
+    hist_file=$(echo $hist_file | sed 's/training_history_//g')
+    hist_file=$(echo $hist_file | sed 's/.csv//g')
+
+    # Make epoch and target epoch the same length
+    epoch=$(printf "%-4s" $epoch)
+    target_epoch=$(printf "%-4s" $target_epoch)
+
+    ic_index=$(echo $hist_file | cut -d "_" -f 4)
+
+
+    # If epoch is equal to target_epoch, we will remove the corresponding IC from the lists above
+    if [ $epoch -eq $target_epoch ]; then
+        # Remove the IC from the lists by creating a new list without the elemen
+        new_bond_ics=()
+        new_angle_ics=()
+        new_dihedral_ics=()
+
+        for ic in "${bond_ics[@]}"; do
+            if [ $ic -ne $ic_index ]; then
+                new_bond_ics+=($ic)
+            fi
+        done
+
+        for ic in "${angle_ics[@]}"; do
+            if [ $ic -ne $ic_index ]; then
+                new_angle_ics+=($ic)
+            fi
+        done
+
+        for ic in "${dihedral_ics[@]}"; do
+            if [ $ic -ne $ic_index ]; then
+                new_dihedral_ics+=($ic)
+            fi
+        done
+
+        bond_ics=("${new_bond_ics[@]}")
+        angle_ics=("${new_angle_ics[@]}")
+        dihedral_ics=("${new_dihedral_ics[@]}")
+    fi
+done
+
+
 # Shuffle the ICs
 bond_ics=($(shuf -e "${bond_ics[@]}"))
 angle_ics=($(shuf -e "${angle_ics[@]}"))
 dihedral_ics=($(shuf -e "${dihedral_ics[@]}"))
+
+echo "Bond ICs left: ${#bond_ics[@]}"
+echo "Angle ICs left: ${#angle_ics[@]}"
+echo "Dihedral ICs left: ${#dihedral_ics[@]}"
 
 # Make folder to store logs
 mkdir -p ./jobs/logs/$SLURM_JOBID
@@ -182,25 +263,11 @@ mkdir -p ./jobs/logs/$SLURM_JOBID/bonds
 mkdir -p ./jobs/logs/$SLURM_JOBID/angles
 mkdir -p ./jobs/logs/$SLURM_JOBID/dihedrals
 
-# for ic in "${bond_ics[@]}"
-# do
-#     sbatch --exclude=fang1,fang8,fang31,fang40,fang54,fang48,fang51,fang52,fang53,fang54 --job-name=B$ic --gres=gpu:1 --mem-per-gpu=11G --nodes=1 --output=./jobs/logs/$SLURM_JOBID/bonds/$ic.log --error=./jobs/logs/$SLURM_JOBID/bonds/$ic.err --wrap="jobs/train_single_no_purge.sh $ic smaug_bond"
-# done
-
-# for ic in "${angle_ics[@]}"
-# do
-#     sbatch --exclude=fang1,fang8,fang31,fang40,fang54,fang48,fang51,fang52,fang53,fang54 --job-name=A$ic --gres=gpu:1 --mem-per-gpu=11G --nodes=1 --output=./jobs/logs/$SLURM_JOBID/angles/$ic.log --error=./jobs/logs/$SLURM_JOBID/angles/$ic.err --wrap="jobs/train_single_no_purge.sh $ic smaug_angle"
-# done
-
-# for ic in "${dihedral_ics[@]}"
-# do
-#     sbatch --exclude=fang1,fang8,fang31,fang40,fang54,fang48,fang51,fang52,fang53,fang54 --job-name=D$ic --gres=gpu:1 --mem-per-gpu=11G --nodes=1 --output=./jobs/logs/$SLURM_JOBID/dihedrals/$ic.log --error=./jobs/logs/$SLURM_JOBID/dihedrals/$ic.err --wrap="jobs/train_single_no_purge.sh $ic smaug_angle"
-# done
-
-
 blength=${#bond_ics[@]}
 alength=${#angle_ics[@]}
 dlength=${#dihedral_ics[@]}
+
+z=0
 
 for (( i=0; i<$blength; i+=4 )); do
     package=("${bond_ics[@]:i:4}")
@@ -224,8 +291,10 @@ for (( i=0; i<$blength; i+=4 )); do
         ic4=""
     fi
 
-    echo Bonds: Starting package $i train for ics: "${package[@]}"
-    sbatch --exclude=fang1,fang51,fang52,fang53,fang54 --gres=gpu:1 --job-name=PB$i --exclusive=user --mem=0 --output=./jobs/logs/$SLURM_JOBID/bonds/$i.log --error=./jobs/logs/$SLURM_JOBID/bonds/$i.err --wrap="jobs/train_single_no_purge.sh smaug_bond $ic1 $ic2 $ic3 $ic4"
+    z=$((z+1))
+
+    echo "[$z]" Bonds: Starting package $i train for ics: "${package[@]}"
+    sbatch --exclude=fang1,fang51,fang52,fang53,fang54 --gpus-per-task=1 --gres=gpu:4 --ntasks=4 --job-name=B$i --exclusive=user --mem=0 --output=./jobs/logs/$SLURM_JOBID/bonds/$i.log --error=./jobs/logs/$SLURM_JOBID/bonds/$i.err --wrap="jobs/train_single_no_purge.sh smaug_bond $ic1 $ic2 $ic3 $ic4"
 done
 
 for (( i=0; i<$alength; i+=4 )); do
@@ -250,9 +319,10 @@ for (( i=0; i<$alength; i+=4 )); do
         ic4=""
     fi
 
+    z=$((z+1))
 
-    echo Angles: Starting package $i train for ics: "${package[@]}"
-    sbatch --exclude=fang1,fang51,fang52,fang53,fang54 --gres=gpu:1 --job-name=PA$i --exclusive=user --mem=0 --output=./jobs/logs/$SLURM_JOBID/angles/$i.log --error=./jobs/logs/$SLURM_JOBID/angles/$i.err --wrap="jobs/train_single_no_purge.sh smaug_angle $ic1 $ic2 $ic3 $ic4"
+    echo "[$z]" Angles: Starting package $i train for ics: "${package[@]}"
+    sbatch --exclude=fang1,fang51,fang52,fang53,fang54 --gpus-per-task=1 --gres=gpu:4 --ntasks=4 --job-name=A$i --exclusive=user --mem=0 --output=./jobs/logs/$SLURM_JOBID/angles/$i.log --error=./jobs/logs/$SLURM_JOBID/angles/$i.err --wrap="jobs/train_single_no_purge.sh smaug_angle $ic1 $ic2 $ic3 $ic4"
 done
 
 for (( i=0; i<$dlength; i+=4 )); do
@@ -277,6 +347,9 @@ for (( i=0; i<$dlength; i+=4 )); do
         ic4=""
     fi
 
-    echo Dihedrals: Starting package $i train for ics: "${package[@]}"
-    sbatch --exclude=fang1,fang51,fang52,fang53,fang54 --gres=gpu:1 --job-name=PD$i --exclusive=user --mem=0 --output=./jobs/logs/$SLURM_JOBID/dihedrals/$i.log --error=./jobs/logs/$SLURM_JOBID/dihedrals/$i.err --wrap="jobs/train_single_no_purge.sh smaug_angle $ic1 $ic2 $ic3 $ic4"
+    z=$((z+1))
+
+    echo "[$z]" Dihedrals: Starting package $i train for ics: "${package[@]}"
+    sbatch --exclude=fang1,fang51,fang52,fang53,fang54 --gpus-per-task=1 --gres=gpu:4 --ntasks=4 --job-name=D$i --exclusive=user --mem=0 --output=./jobs/logs/$SLURM_JOBID/dihedrals/$i.log --error=./jobs/logs/$SLURM_JOBID/dihedrals/$i.err --wrap="jobs/train_single_no_purge.sh smaug_angle $ic1 $ic2 $ic3 $ic4"
+    # sbatch --exclude=fang1,fang51,fang52,fang53,fang54 --gres=gpu:1 --job-name=D$i --exclusive=user --mem=0 --output=./jobs/logs/$SLURM_JOBID/dihedrals/$i.log --error=./jobs/logs/$SLURM_JOBID/dihedrals/$i.err --wrap="jobs/train_single_no_purge.sh smaug_angle $ic1 $ic2 $ic3 $ic4"
 done
